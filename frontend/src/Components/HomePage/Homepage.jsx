bd41e5960911e87fb767c2f82542d69e5c18c679
Yassinos <castroyassine0@gmail.com>
2024-01-05 14:24:10 +0100

    Still Working On It

--- frontend/src/Components/HomePage/Homepage.jsx
+++ frontend/src/Components/HomePage/Homepage.jsx
@@ -14,19 +14,60 @@ const Homepage = () => {
         </div>
         <div className="headerNavs">
           <nav>
-            <a href="#Home" className="navLinks">Home</a>
-            <a href="#Projects" className="navLinks">Projects</a>
-            <a href="#Certs" className="navLinks">Certifications</a>
-            <a href="#Experiences" className="navLinks">Experiences</a>
-            <a href="resume.pdf" className="navLinks">Download Resume</a>
+            <a href="#Home" className="navLinks">
+              Home
+            </a>
+            <a href="#Projects" className="navLinks">
+              Projects
+            </a>
+            <a href="#Certs" className="navLinks">
+              Certifications
+            </a>
+            <a href="#Experiences" className="navLinks">
+              Experiences
+            </a>
+            <a href="resume.pdf" className="navLinks">
+              Download Resume
+            </a>
           </nav>
         </div>
       </header>
       <main className="main">
-        <section id="Home" className="section1 sections"></section>
-        <section id="Projects" className="section2 sections"></section>
-        <section id="Certs" className="section3 sections"></section>
-        <section id="Experiences" className="section4 sections"></section>
+        <div className="portfolio">
+          <div className="part1">
+            <img
+              src={logo}
+              alt="Yassine Castro aka Yassinos aka DMFGenius aka Yassinos-Coder aka zeus"
+            />
+            <p>Yassine Castro</p>
+            <a href="https://www.linkedin.com/in/yassine-castro-6a6ba7237/">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/Yassinos-coder">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="https://www.instagram.com/yassinos.js/">
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+            <a href="https://www.twitch.tv/yassinos08">
+              <i className="fa-brands fa-twitch"></i>
+            </a>
+          </div>
+          <div className="part2">
+            <h1>About Me</h1>
+            <p>
+              Hello! I{"'"}m Yassine Castro, a passionate geek deeply immersed
+              in the tech world. As a proficient Full Stack Developer, I excel
+              in MERN and React-Native, constantly pushing the boundaries of
+              what
+              {"'"}s possible. My dedication to hard work and responsibility is
+              matched only by my commitment to continual improvement. I thrive
+              on adventure, both in coding and life, always seeking new
+              challenges and innovative solutions. Let{"'"}s embark on a journey
+              where technology meets creativity, and ideas turn into reality.
+            </p>
+          </div>
+        </div>
       </main>
     </div>
   );
