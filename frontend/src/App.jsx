598c0ffd2ccce06375a4e8a82cd2f1185f837e32
Yassinos <castroyassine0@gmail.com>
2024-01-05 13:26:02 +0100

    Under Dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Homepage from './Components/HomePage/Homepage'
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index Component={Homepage} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/Components/HomePage/Homepage.jsx
+++ frontend/src/Components/HomePage/Homepage.jsx
@@ -1,12 +1,9 @@
 import "./HomePage.css";
-import React from "react";
-import { FaLinkedin, FaGithub, FaFileDownload } from "react-icons/fa";
-import { RiInstagramFill } from "react-icons/ri";
-import logoPerson from "../../Assets/image/logo.png";
+
 const Homepage = () => {
   return (
     <div className="container">
-        Coming Soon!
+        <h1>Coming Soon !</h1>
     </div>
   );
 };
